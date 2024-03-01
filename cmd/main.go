aea3ce1e2a0f76f3bce2966b990cbc598639e444
01001010:01001011 <33685667+jithinkunjachan@users.noreply.github.com>
2024-03-01 17:19:58 +0100

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+func main() {
+
+	user := os.Getenv("DYNU_USERNAME")
+	if user == "" {
+		log.Fatal("please provide DYNU_USERNAME")
+	}
+	pwd := os.Getenv("DYNU_PASSWORD")
+	if pwd == "" {
+		log.Fatal("please provide DYNU_PASSWORD")
+	}
+
+	host := os.Getenv("DYNU_HOST")
+	if host == "" {
+		host = "api.dynu.com"
+	}
+
+	h := md5.New()
+	io.WriteString(h, pwd)
+	u := url.URL{
+		Scheme: "http",
+		Host:   host,
+		Path:   "nic/update",
+	}
+	q := u.Query()
+	q.Add("username", user)
+	q.Add("password", fmt.Sprintf("%x", h.Sum(nil)))
+	u.RawQuery = q.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		log.Fatalf("error while creating request %v", err)
+	}
+
+	c := http.Client{
+		Timeout: time.Second * 20,
+	}
+	for {
+		resp, err := c.Do(req)
+		if err != nil {
+			log.Fatalf("error while quering dynu %v", err)
+		}
+		if resp.StatusCode != 200 {
+			log.Fatalf("error while updating ip address status code [%v]", resp.StatusCode)
+		}
+		resp.Body.Close()
+		log.Println("successfully updated")
+		time.Sleep(time.Second * 50)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.comjithinkunjachan
+
+go 1.20
